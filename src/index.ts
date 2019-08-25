52ad7720b6927ba84a665b9f4e39e6949380401c
Taylor Growden <taylorgrowden@gmail.com>
2019-08-25 15:08:09 -0400

    update logic to accept multiple input types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-import { Feature, FeatureCollection } from 'geojson'
 import { convertCoordinate, convertTimestampMs } from './lib/converters'
+
+export interface Feature {
+	type: 'Feature'
+	geometry: {
+		type: 'Point'
+		coordinates: number[]
+	}
+	id?: string | number
+	properties: {
+		[key: string]: any
+	} | null
+}
 
 export interface TakeoutLocationHistory {
 	locations: TakeoutLocation[]
@@ -20,34 +31,44 @@ interface TakeoutActivityMeta {
 	confidence: number
 }
 
-export function takeoutToGeoJSON(
-	data: TakeoutLocationHistory
-): FeatureCollection {
-	const features: Feature[] = data.locations.map(location => {
-		const { accuracy, activity } = location
-		const coordinates = [
-			convertCoordinate(location.longitudeE7, 'longitude'),
-			convertCoordinate(location.latitudeE7, 'latitude')
-		]
-
-		return {
-			type: 'Feature',
-			geometry: {
-				type: 'Point',
-				coordinates
-			},
-			properties: {
-				date: convertTimestampMs(location.timestampMs),
-				accuracy,
-				activity
-			}
-		}
-	})
+function convertLocation(location: TakeoutLocation): Feature {
+	const { accuracy, activity } = location
+	const coordinates = [
+		convertCoordinate(location.longitudeE7, 'longitude'),
+		convertCoordinate(location.latitudeE7, 'latitude')
+	]
 
 	return {
-		type: 'FeatureCollection',
-		features
+		type: 'Feature',
+		geometry: {
+			type: 'Point',
+			coordinates
+		},
+		properties: {
+			date: convertTimestampMs(location.timestampMs),
+			accuracy,
+			activity
+		}
 	}
 }
 
-export default takeoutToGeoJSON
+export function convert(data: TakeoutLocationHistory): Feature[]
+export function convert(data: TakeoutLocation[]): Feature[]
+export function convert(date: TakeoutLocation): Feature
+export function convert(
+	data: TakeoutLocationHistory | TakeoutLocation[] | TakeoutLocation
+) {
+	if (Array.isArray(data)) {
+		return data.map(convertLocation)
+	}
+
+	if ('locations' in data) {
+		return data.locations.map(convertLocation)
+	}
+
+	return convertLocation(data)
+}
+
+export default convert
+
+module.exports = convert
